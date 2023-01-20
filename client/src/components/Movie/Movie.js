e0e74798295e1d619c0296ae538e17d773f392ab
Hamayoon <hamayoonk38@gmail.com>
2023-01-20 22:15:56 +0000

    Movie page redesigned

--- client/src/components/Movie/Movie.js
+++ client/src/components/Movie/Movie.js
@@ -1,27 +1,35 @@
 import React from "react";
 import "./Movie.css";
-import noimage from '../../assets/no-image.png'
+import noimage from "../../assets/no-image.png";
 
 const Movie = (props) => {
-    const words = props.overview.split(" ");
-    const reducedWords = words.slice(0, 40);
-    const reducedDescription = reducedWords.join(" ");
+  const words = props.overview.split(" ");
+  const reducedWords = words.slice(0, 50);
+  const reducedDescription = reducedWords.join(" ");
   return (
-    <div className="m-movie">
-    <div className="m-poster-container">
-      <img
-        className="m-poster"
-        src={props.poster_path ? "https://image.tmdb.org/t/p/original/" + props.poster_path : noimage}
-        alt={props.title}
-      />
+    <div className="movie-card">
+      <div className="m-poster-container">
+        <img
+          className="m-poster"
+          src={
+            props.poster_path
+              ? "https://image.tmdb.org/t/p/original/" + props.poster_path
+              : noimage
+          }
+          alt={props.title}
+        />
       </div>
-      <div className="m-info">
-      <div className="m-title">
-        {props.title} ({props.release_date})
-      </div>
-      <div className="m-rating">{props.vote_average}</div>
-      <div className="m-genre">{props.genres.split("-").join(", ")}</div>
-      <div className="m-overview">{reducedDescription} {words.length > 40 && "..."}</div>
+      <div className="m-info-container">
+        <div className="m-title-rating">
+          <div className="m-title">
+            {props.title} ({props.release_date})
+          </div>
+          <div className="m-rating">{props.vote_average}</div>
+        </div>
+        <div className="m-genre">{props.genres.split("-").join(", ")}</div>
+        <div className="m-description">
+          {props.overview}
+        </div>
       </div>
     </div>
   );
